d0e3f22bbf5a1182e6f8af40421de3fe62046084
ERIC <choieric00@gmail.com>
2023-09-16 22:25:38 +0900

    fourth commit

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -1,5 +1,7 @@
 package com.mycom.word;
 
+import javax.management.RuntimeErrorException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -25,7 +27,7 @@ public class WordCRUD implements ICRUD{
         return new Word(0, level, word, meaning);
     }
 
-    public void addWord() {
+    public void addItem() {
         Word one = (Word)add();
         list.add(one);
         System.out.println("새 단어가 단어장에 추가되었습니다. ");
@@ -74,24 +76,25 @@ public class WordCRUD implements ICRUD{
         return idlist;
     }
 
-    public void listAll(int level) {
-        int j=0;
+
+        public void listAll(int level){
+            int j= 0;
             System.out.println("-----------------------------");
             for(int i = 0; i< list.size(); i++) {
                 int ilevel = list.get(i).getLevel();
-                if(ilevel != level) continue;
-
-                System.out.print((j+1) + " ");
+                if (ilevel != level) continue;
+                System.out.print((j + 1) + " ");
                 System.out.println(list.get(i).toString());
+                j++;
             }
-            System.out.println("-----------------------------");
-        }
+            }
 
-    }
+
+
 
 /*
     public void updateItem() {
-        System.out.println("=> 수정할 단어 검색 : ");
+        System.out.print("=> 수정할 단어 검색 : ");
         String keyword = s.next();
         ArrayList<Integer> idlist = this.listAll(keyword);
         System.out.print("=> 수정할 번호 선택 : ");
@@ -102,12 +105,12 @@ public class WordCRUD implements ICRUD{
         String meaning = s.nextLine();
         Word word = list.get(idlist.get(id-1));
         word.setMeaning(meaning);
-        System.out.print("단어가 수정되었습니다. ");
+        System.out.println("단어가 수정되었습니다. ");
     }
 
 
     public void deleteItem() {
-        System.out.println("=> 삭제할 단어 검색 : ");
+        System.out.print("=> 삭제할 단어 검색 : ");
         String keyword = s.next();
         ArrayList<Integer> idlist = this.listAll(keyword);
         System.out.print("=> 삭제할 번호 선택 : ");
@@ -128,21 +131,26 @@ public class WordCRUD implements ICRUD{
         try{
             BufferedReader br = new BufferedReader(new FileReader(fname));
             String line;
-
+            int count = 0;
             while(true) {
                 line = br.readLine();
                 if(line == null) break;
 
-                String date[] = line.split("\\|");
+
+                String data[] = line.split("\\|");
                 int level = Integer.parseInt(data[0]);
                 String word = data[1];
                 String meaning = data[2];
                 list.add(new Word(0, level, word, meaning));
+                count++;
             }
             br.close();
-            system.out.println("");
-        } catch (IOException e){
+            System.out.println("==> " + count + "개 로딩 완료!!!");
+        }
+        catch (FileNotFoundException e){
             e.printStackTrace();
+        } catch (IOException e){
+            throw new RuntimeException(e);
         }
 
     }
@@ -151,12 +159,12 @@ public class WordCRUD implements ICRUD{
 
         public void saveFile() {
             try {
-                PrintWriter pr = new PrintWriter(new FileWriter("text.txt"));
+                PrintWriter pr = new PrintWriter(new FileWriter("fname"));
                 for (Word one : list) {
                     pr.write(one.toFileString() + "\n");
                 }
                 pr.close();
-                Systme.out.println("==> 데이터 저장 완료 !!!");
+                System.out.println("==> 데이터 저장 완료 !!!");
 
 
             } catch (IOException e) {
@@ -170,4 +178,12 @@ public class WordCRUD implements ICRUD{
             listAll(level);
         }
 
- */
+        public void searchWord() {
+            System.out.print("=> 원하는 단어는? ");
+            String keyword = s.next();
+            listAll(keyword);
+        }
+        */
+
+}
+
